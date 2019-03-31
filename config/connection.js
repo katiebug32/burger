2c4ff076370c799ba02153bb72c01d34fc39c471
katiebug32 <katiebug32@gmail.com>
2019-03-31 00:14:26 -0500

    heroku deploy changes

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 // var pswd = require("./pswd.js");
 // console.log("Pass ", process.env.PSWD)
 // Set up our connection information
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.PSWD,
   database: "burgers_db"
 });
+};
 
 // Connect to the database
 connection.connect(function(err) {
